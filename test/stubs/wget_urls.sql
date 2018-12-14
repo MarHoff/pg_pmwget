e2e3ed2a2cbf7d7e56c19622120a2d27d873b0b1
Martin Hoffmann <dev@marhoff.xyz>
2018-12-14 18:39:36 +0100

    Adding a simple php API for testing

--- test/stubs/wget_urls.sql
+++ test/stubs/wget_urls.sql
@@ -1,13 +1,13 @@
 WITH tsel AS (
-  SELECT array_agg(format('https://hacker-news.firebaseio.com/v0/item/%s.json',id)) qarray
+  SELECT array_agg(format('http://pmwget.test/pg_pmwget_test.php?q=%s&e=30',id)) qarray
   FROM (
-    SELECT generate_series(1,10)::bigint id
+    SELECT generate_series(1,100)::bigint id
   )foo
 )
 
 
 SELECT *
-FROM wget_urls((SELECT qarray FROM tsel), workers := 10, timeout := 5)
+FROM wget_urls((SELECT qarray FROM tsel)::url_array, b_parallel_jobs := 20, b_timeout := 5)
 ORDER BY retries desc, batch asc;
 
 --SELECT * FROM wget_urls_raw('https://hacker-news.firebaseio.com/v0/item/7897.json https://hacker-news.firebaseio.com/v0/item/7898.json https://hacker-news.firebaseio.com/v0/item/7899.json', workers := 100);
