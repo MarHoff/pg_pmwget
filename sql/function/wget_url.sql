cdbef202d2d3d89f8947179e277eb60aa47ea88d
Martin Hoffmann <dev@marhoff.xyz>
2018-12-29 12:04:18 +0100

    Setting better defaults and headers for functions

--- sql/function/wget_url.sql
+++ sql/function/wget_url.sql
@@ -1,18 +1,27 @@
 -- Function: wget_url(text, numeric, numeric, integer)
 
--- DROP FUNCTION @extschema@.wget_url(text, numeric, numeric, integer);
-
-CREATE OR REPLACE FUNCTION @extschema@.wget_url(
+CREATE OR REPLACE FUNCTION @extschema@.wget_url
+(
     url @extschema@.url,
     min_latency double precision DEFAULT 0,
     timeout double precision DEFAULT 5,
     tries integer DEFAULT 1,
-    waitretry double precision DEFAULT 10
-    )
+    waitretry double precision DEFAULT 0
+
+
+
+  )
   RETURNS text AS
 $BODY$
 #!/bin/sh
-sleep $2 & wget --timeout=$3 --tries=$4 --waitretry=$5 -qO- "$1" & wait
+export HOME='/tmp'
+export WGET_URL="$1"
+export WGET_MIN_LATENCY=$2
+export WGET_TIMEOUT=$3
+export WGET_TRIES=$4
+export WGET_WAITRETRY=$5
+
+sleep $WGET_MIN_LATENCY & wget --timeout=$WGET_TIMEOUT --tries=$WGET_TRIES --waitretry=$WGET_WAITRETRY -qO- $WGET_URL & wait
 $BODY$
   LANGUAGE plsh VOLATILE
   PARALLEL SAFE
--- sql/function/wget_urls_raw.sql
+++ sql/function/wget_urls_raw.sql
@@ -1,14 +1,12 @@
 -- Function: wget_urls(text, numeric, numeric, integer)
 
--- DROP FUNCTION @extschema@.wget_urls(text, numeric, numeric, integer);
-
 CREATE OR REPLACE FUNCTION @extschema@.wget_urls_raw
 (
     url_shlist @extschema@.url_shlist ,
     min_latency double precision DEFAULT 0,
     timeout double precision DEFAULT 5,
     tries integer DEFAULT 1,
-    waitretry double precision DEFAULT 10,
+    waitretry double precision DEFAULT 0,
     parallel_jobs integer DEFAULT 10,
     delimiter text DEFAULT '@wget_token@',
     delay double precision DEFAULT 0
@@ -17,15 +15,17 @@ CREATE OR REPLACE FUNCTION @extschema@.wget_urls_raw
 $BODY$
 #!/bin/sh
 export HOME='/tmp'
-export WGET_STRING="$1"
-export WGET_WAIT=$2
+export WGET_URL_SHLIST="$1"
+export WGET_MIN_LATENCY=$2
 export WGET_TIMEOUT=$3
 export WGET_TRIES=$4
-export WGET_WAITRERY=$5
-export PARALLEL_JOBS=$6
-export PMWGET_DELIMITER=$7
-sleep $8
-sleep $2 & parallel --jobs $PARALLEL_JOBS 'echo -n {};echo -n $PMWGET_DELIMITER; START_TIME=$(date +"%F %T.%6N"); wget --timeout=$WGET_TIMEOUT --tries=$WGET_TRIES --waitretry=$WGET_WAITRERY -qO- {}; END_TIME=$(date +"%F %T.%6N"); echo $PMWGET_DELIMITER$START_TIME$PMWGET_DELIMITER$END_TIME$PMWGET_DELIMITER$PMWGET_DELIMITER' ::: $WGET_STRING & wait
+export WGET_WAITRETRY=$5
+export WGET_PARALLEL_JOBS=$6
+export WGET_DELIMITER="$7"
+export WGET_DELAY=$8
+
+sleep $WGET_DELAY
+sleep $WGET_MIN_LATENCY & parallel --jobs $WGET_PARALLEL_JOBS 'echo -n {};echo -n $WGET_DELIMITER; START_TIME=$(date +"%F %T.%6N"); wget --timeout=$WGET_TIMEOUT --tries=$WGET_TRIES --waitretry=$WGET_WAITRETRY -qO- {}; END_TIME=$(date +"%F %T.%6N"); echo $WGET_DELIMITER$START_TIME$WGET_DELIMITER$END_TIME$WGET_DELIMITER$WGET_DELIMITER' ::: $WGET_URL_SHLIST & wait
 #TODO quickly explain parallel usage
 $BODY$
   LANGUAGE plsh VOLATILE
